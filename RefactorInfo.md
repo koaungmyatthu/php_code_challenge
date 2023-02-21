c686cb1a220599d86e0ea352f14a9e7c3db97166
aungmyatthu <aungmyatthu07@gmail.com>
2023-02-21 23:02:42 +0630

    Refactor code for FinalResult.php

---
+++ RefactorInfo.md
@@ -0,0 +1,8 @@
+# Refactor info for FinalResult.php #
+
+1. Add the PHPDoc comments.
+2. Use the constant variable to know the index name of csv file.
+3. Add the throw error exception for the fail process.
+4. Change the variable name to be meaningful and use the camelCase format.
+5. Refactor code by separating the smaller functions to work the one function one task.
+6. Add the fclose() to close the csv file.
--- src/FinalResult.php
+++ src/FinalResult.php
@@ -1,40 +1,159 @@
 <?php
 
-class FinalResult {
-    function results($f) {
-        $d = fopen($f, "r");
-        $h = fgetcsv($d);
-        $rcs = [];
-        while(!feof($d)) {
-            $r = fgetcsv($d);
-            if(count($r) == 16) {
-                $amt = !$r[8] || $r[8] == "0" ? 0 : (float) $r[8];
-                $ban = !$r[6] ? "Bank account number missing" : (int) $r[6];
-                $bac = !$r[2] ? "Bank branch code missing" : $r[2];
-                $e2e = !$r[10] && !$r[11] ? "End to end id missing" : $r[10] . $r[11];
-                $rcd = [
-                    "amount" => [
-                        "currency" => $h[0],
-                        "subunits" => (int) ($amt * 100)
-                    ],
-                    "bank_account_name" => str_replace(" ", "_", strtolower($r[7])),
-                    "bank_account_number" => $ban,
-                    "bank_branch_code" => $bac,
-                    "bank_code" => $r[0],
-                    "end_to_end_id" => $e2e,
-                ];
-                $rcs[] = $rcd;
-            }
+/**
+ * Class FinalResult
+ * 
+ * @copyright (c) 2023 - Hivelocity inc, All Right Reserved.
+ * 
+ */
+class FinalResult
+{
+    const CSV_HEADER = [
+        'currency' => 0,
+        'failure_code' => 1,
+        'failure_message' => 2
+    ];
+
+    const CSV_BODY = [
+        'bank_code' => 0,
+        'bank_branch_code' => 2,
+        'bank_account_number' => 6,
+        'bank_account_name' => 7,
+        'amount' => 8,
+        'first_end_to_end_id' => 10,
+        'last_end_to_end_id' => 11
+    ];
+
+    /**
+     * Retrieve data from CSV file.
+     * 
+     * @param string $filePath
+     * 
+     * @return array
+     */
+    public function results(string $fileName)
+    {
+        if (!file_exists($fileName)) {
+            throw new Exception('File not found.');
         }
-        $rcs = array_filter($rcs);
+
+        $csvFile = fopen($fileName, "r");
+        if (!$csvFile) {
+            throw new Exception('File open failed.');
+        }
+
+        $header = fgetcsv($csvFile);
+        if (!$header) {
+            throw new Exception('Fail to read the header info.');
+        }
+
+        $records = $this->getRecordsFromCsvFile($header, $csvFile);
+
+        fclose($csvFile);
+
         return [
-            "filename" => basename($f),
-            "document" => $d,
-            "failure_code" => $h[1],
-            "failure_message" => $h[2],
-            "records" => $rcs
+            "filename" => basename($fileName),
+            "document" => $csvFile,
+            "failure_code" => $header[self::CSV_HEADER['failure_code']],
+            "failure_message" => $header[self::CSV_HEADER['failure_message']],
+            "records" => $records
         ];
     }
-}
 
-?>
+    /**
+     * Get records from CSV file.
+     * 
+     * @param $header,
+     * @param $csvFile
+     * 
+     * @return array
+     */
+    private function getRecordsFromCsvFile($header, $csvFile)
+    {
+        $records = [];
+        while (!feof($csvFile)) {
+            $row = fgetcsv($csvFile);
+            if (count($row) === 16) {
+                $records[] = $this->getRecordFromCsvFile($header, $row);
+            }
+        }
+
+        return $records;
+    }
+
+    /**
+     * Get record from csv file.
+     * 
+     * @param array $header
+     * @param array $row
+     * 
+     * @return array
+     */
+    private function getRecordFromCsvFile($header, $row)
+    {
+        $amount = $this->getAmount($row);
+        $bankAccountNumber = $this->getBankAccountNumber($row);
+        $bankBranchCode = $this->getBankBranchCode($row);
+        $endToEndID = $this->getEndToEndID($row);
+
+        return [
+            "amount" => [
+                "currency" => $header[self::CSV_HEADER['currency']],
+                "subunits" => (int) ($amount * 100)
+            ],
+            "bank_account_name" => str_replace(" ", "_", strtolower($row[self::CSV_BODY['bank_account_name']])),
+            "bank_account_number" => $bankAccountNumber,
+            "bank_branch_code" => $bankBranchCode,
+            "bank_code" => $row[self::CSV_BODY['bank_code']],
+            "end_to_end_id" => $endToEndID,
+        ];
+    }
+
+    /**
+     * Get the amount.
+     * 
+     * @param $row
+     * 
+     * @return int|float
+     */
+    private function getAmount(array $row)
+    {
+        return (!$row[self::CSV_BODY['amount']] || $row[self::CSV_BODY['amount']] === "0") ? 0 : (float) $row[self::CSV_BODY['amount']];
+    }
+
+    /**
+     * Get the Bank account number.
+     * 
+     * @param array $row
+     * 
+     * @return int|string
+     */
+    private function getBankAccountNumber(array $row)
+    {
+        return !$row[self::CSV_BODY['bank_account_number']] ? "Bank account number missing" : (int) $row[self::CSV_BODY['bank_account_number']];
+    }
+
+    /**
+     * Get the bank branch code.
+     * 
+     * @param array $row
+     * 
+     * @return int|string
+     */
+    private function getBankBranchCode(array $row)
+    {
+        return !$row[self::CSV_BODY['bank_branch_code']] ? "Bank branch code missing" : $row[self::CSV_BODY['bank_branch_code']];
+    }
+
+    /**
+     * Get the end to end id.
+     * 
+     * @param array $row
+     * 
+     * @return string
+     */
+    private function getEndToEndID(array $row)
+    {
+        return !$row[self::CSV_BODY['first_end_to_end_id']] && !$row[self::CSV_BODY['last_end_to_end_id']] ? "End to end id missing" : $row[self::CSV_BODY['first_end_to_end_id']] . $row[self::CSV_BODY['last_end_to_end_id']];
+    }
+}
